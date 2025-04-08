ab274da1912882464d2c0ce819c3e1a82ed49a38
Eyal Pasha <eyalpasha115@outlook.com>
2025-04-08 18:31:45 +0300

    policies updates

--- backend/config.py
+++ backend/config.py
@@ -19,7 +19,8 @@ CORS_ORIGINS = os.getenv("CORS_ORIGINS",
     "http://localhost:5500,http://127.0.0.1:5500,"
     "http://localhost:8001,http://127.0.0.1:8001,http://10.0.0.28:8001,"
     "http://localhost:8080,http://127.0.0.1:8080,"
-    "http://10.0.0.28:5500,"
+    "http://10.0.0.28:5500,http://10.0.0.67:5500,"
+    "http://10.0.0.67:8001,http://10.0.0.67:8000,"
     "http://localhost:3000,http://127.0.0.1:3000,"
     "http://localhost:8000,http://127.0.0.1:8000,"
     "null"  # For local file access
